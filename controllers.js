63a010e978c9a6e6d72ef498e441351dfad66d20
heidi <hayriye_oeztuerk@web.de>
2021-10-09 11:38:19 +0200

    commit

--- controllers.js
+++ controllers.js
@@ -11,6 +11,24 @@ const uri = process.env.DATABASE_URL
 console.log(uri)
 const db = pgp(uri)
 
+const pgp = require('pg-promise')()
+let db
+
+if (process.env.DATABASE_URL) {
+    db = pgp({
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false }
+    })
+} else {
+    const username = process.env.DB_USER
+    const password = process.env.DB_PASS
+    const host = process.env.DB_HOST
+    const port = process.env.DB_PORT
+
+    let uri = `postgres://${username}:${password}@${host}:${port}/library2`
+    db = pgp(uri)
+}
+
 async function addBook(book){
     const result = await db.query('INSERT INTO books(${this:name}) VALUES(${this:csv})', book)
     return book;
